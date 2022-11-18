3f58ae970e32d9cd45ccdae22a907937504f0561
zhuba-Ahhh <3477826311@qq.com>
2022-11-18 15:20:08 +0800

    mall-demo

---
+++ mall-demo/README.md
@@ -0,0 +1,3 @@
+# mall-demo
+
+**Vue3 + Ts**
---
+++ mall-demo/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="common-layout">
+    <TopNavBar/>
+    <div class="main-container">
+      <LeftSideBar :menu_list="need_list"/>
+      <div style="flex: 1;">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TopNavBar from "@/components/TopNavBar.vue";
+import LeftSideBar from "@/components/LeftSideBar.vue";
+import {useRouter} from "vue-router";
+export default defineComponent({
+  components: {
+    TopNavBar,
+    LeftSideBar
+  },
+  setup () {
+    const router = useRouter()
+    const need_list = router.getRoutes().filter(v => v.meta.isShow)
+    //console.log(need_list)
+    return {
+      need_list
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+  .main-container{
+    display: flex;
+  }
+</style>
